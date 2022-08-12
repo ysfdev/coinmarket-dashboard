86f5caf84ce53be658d9f6bb98255a624e953db3
ysfdev <13709931+ysfdev@users.noreply.github.com>
2022-08-12 18:43:46 -0400

    Coin data client (POC) (#9)
    * Initial data HTTP client setup
    
    * Market API Client updates and improvements
    
    * small clean ups
    
    * Update data response type
    
    * update default API host

---
+++ app/MarketDataClient.hs
@@ -0,0 +1,58 @@
+module MarketDataClient
+  ( fetchData,
+    fetchLatestListings,
+    doMarketReq,
+  )
+where
+
+import Control.Exception (SomeException (SomeException), handle)
+import Control.Monad (MonadPlus (mzero))
+import Control.Monad.IO.Class (liftIO)
+import Data.Aeson as JSON
+import Data.String (IsString (fromString))
+import qualified GHC.Exts as Aeson
+import MarketDataClientTypes
+import MarketDataClientUtils
+import Network.HTTP.Client.Conduit (HttpException)
+import qualified Network.HTTP.Conduit as Http
+import qualified Network.HTTP.Simple as Http
+
+-- Public METHODS --
+-- fetchData queries market data from custom endpoints
+fetchData :: ReqParams -> IO DataResponse
+fetchData rp = handle reqExHandler req
+  where
+    req = do
+      res <- doMarketReq rp
+      resData res
+
+-- fetchLatestListing returns a paginated list of all active cryptocurrencies with latest market data
+fetchLatestListings :: QueryParams -> IO DataResponse
+fetchLatestListings q =
+  fetchData
+    ReqParams
+      { host = coinMarketAPIServerHost,
+        apiKey = coinMarketAPIKey,
+        endpoint = cryptoLatestListingsEndpoint,
+        query = q
+      }
+
+-- doMarketReq constructs and sends a coin marketcap API request
+-- TODO: handle JSONConversionException
+doMarketReq :: ReqParams -> IO (Http.Response APIResponse)
+doMarketReq rp = do
+  request' <- Http.parseRequest (rHost rp)
+  let request =
+        Http.setRequestPath (rEndpoint rp) $
+        Http.setRequestQueryString (rqQueryStr rp) $
+        Http.setRequestSecure True $
+        Http.addRequestHeader (fromString "X-CMC_PRO_API_KEY") (fromString $ apiKey rp) $
+        Http.addRequestHeader
+          (fromString "Accept-Encoding")
+          (fromString "deflate, gzip")
+        request'
+  print $ "Sending new market API request to: " ++ rHost rp ++ "" ++ rEndpoint rp
+  print request
+  response <- Http.httpJSON request
+  putStrLn $ "Response status code: " ++ show (Http.getResponseStatusCode response)
+  return response
---
+++ app/MarketDataClientTypes.hs
@@ -0,0 +1,91 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module MarketDataClientTypes where
+
+import Control.Exception (SomeException (SomeException), handle)
+import Control.Monad (MonadPlus (mzero))
+import Control.Monad.IO.Class (liftIO)
+import Data.Aeson as JSON
+import Data.String (IsString (fromString))
+import qualified GHC.Exts as Aeson
+import Network.HTTP.Client.Conduit (HttpException)
+import qualified Network.HTTP.Conduit as Http
+import qualified Network.HTTP.Simple as Http
+import Control.Applicative (Alternative(empty))
+
+-- CoinMarketCap API Details
+-- Hosts ---
+coinMarketAPIServerHost = "https://pro-api.coinmarketcap.com"
+
+coinMarketSandboxAPIServerHost = "https://sandbox-api.coinmarketcap.com"
+
+coinMarketAPIKey = "882a4874-8e57-4674-8b9d-7509be621718"
+
+coinMarketSandboxAPIKey = "b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c"
+
+-- Endpoints --
+cryptoLatestListingsEndpoint = "/v1/cryptocurrency/listings/latest"
+
+-- Data Types Definitions --
+type Endpoint = String
+
+type Host = String
+
+type APIKey = String
+
+type StatusCode = String
+
+type Message = String
+
+type RawReqBody = String
+
+type Limit = Int
+
+type RStart = Int
+
+data QueryParams = QueryParams
+  { -- start allows to offset the start (1-based index) of the paginated list of items to return.
+    start :: RStart,
+    -- limit allows to specify the number of results to return. Default: 100
+    limit :: Limit
+  }
+
+data ReqParams = ReqParams
+  { host :: Host,
+    endpoint :: Endpoint,
+    query :: QueryParams,
+    apiKey :: APIKey
+  }
+
+data Status
+  = Successful
+  | Unauthorized
+  | Forbidden
+  | BadRequest
+  | InternalServerError
+  | TooManyRequests
+  | RequestRedirected
+  | FailToSendReq
+  deriving (Show)
+
+data DataResponse = DataResponse
+  { status :: Status,
+    message :: Message, --- (error_message from API response)
+    body :: Array  -- API response.data body
+  }
+  deriving (Show)
+
+data APIResponse = APIResponse
+  { rmessage :: Message,
+    rdata :: Array
+  }
+  deriving (Show)
+
+instance FromJSON APIResponse where
+  parseJSON (Object v) = do
+    data' <- v .:? "data" .!= empty
+    -- TODO extract status.error_message
+    -- status' <- v .:? "status" .!= Object
+    -- msg'  <- status' .:? "error_message"
+    return $ APIResponse {rdata = data', rmessage = ""}
+  parseJSON _ = mzero
