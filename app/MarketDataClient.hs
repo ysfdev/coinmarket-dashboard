e8947ad113ef9b59f537ea73b43f44341df61fb6
Yeramin <13709931+ysfdev@users.noreply.github.com>
2022-08-12 17:43:03 -0400

    small clean ups

--- app/MarketDataClient.hs
+++ app/MarketDataClient.hs
@@ -1,49 +1,55 @@
-module MarketDataClient (
-  fetchData,
-  fetchLatestListings,
-  doMarketReq
-) where
+module MarketDataClient
+  ( fetchData,
+    fetchLatestListings,
+    doMarketReq,
+  )
+where
 
-import MarketDataClientUtils
-import MarketDataClientTypes
-
-import Data.Aeson as JSON
-import qualified Network.HTTP.Simple as Http
-import Control.Monad (MonadPlus(mzero))
-import Data.String (IsString(fromString))
-import Control.Exception (SomeException(SomeException), handle)
-import Network.HTTP.Client.Conduit (HttpException)
-import qualified GHC.Exts as Aeson
-import qualified Network.HTTP.Conduit as Http
+import Control.Exception (SomeException (SomeException), handle)
+import Control.Monad (MonadPlus (mzero))
 import Control.Monad.IO.Class (liftIO)
+import Data.Aeson as JSON
+import Data.String (IsString (fromString))
+import qualified GHC.Exts as Aeson
+import MarketDataClientTypes
+import MarketDataClientUtils
+import Network.HTTP.Client.Conduit (HttpException)
+import qualified Network.HTTP.Conduit as Http
+import qualified Network.HTTP.Simple as Http
 
 -- Public METHODS --
 -- fetchData queries market data from custom endpoints
-fetchData :: ReqParams -> IO DataResponse 
-fetchData rp = handle reqExHandler req where
+fetchData :: ReqParams -> IO DataResponse
+fetchData rp = handle reqExHandler req
+  where
     req = do
       res <- doMarketReq rp
       resData res
-  
--- fetchLatestListing returns a paginated list of all active cryptocurrencies with latest market data
-fetchLatestListings :: QueryParams ->  IO DataResponse
-fetchLatestListings q =  fetchData ReqParams { 
-  host = coinMarketSandboxAPIServerHost,
-  apiKey= coinMarketSandboxAPIKey,
-  endpoint = cryptoLatestListingsEndpoint, 
-  query = q
-}
 
--- doMarketReq constructs and sends a coin marketcap API request 
+-- fetchLatestListing returns a paginated list of all active cryptocurrencies with latest market data
+fetchLatestListings :: QueryParams -> IO DataResponse
+fetchLatestListings q =
+  fetchData
+    ReqParams
+      { host = coinMarketSandboxAPIServerHost,
+        apiKey = coinMarketSandboxAPIKey,
+        endpoint = cryptoLatestListingsEndpoint,
+        query = q
+      }
+
+-- doMarketReq constructs and sends a coin marketcap API request
 -- TODO: handle JSONConversionException
 doMarketReq :: ReqParams -> IO (Http.Response APIResponse)
 doMarketReq rp = do
   request' <- Http.parseRequest (rHost rp)
-  let request = Http.setRequestPath (rEndpoint rp)
-        $ Http.setRequestQueryString (rqQueryStr rp)
-        $ Http.setRequestSecure True
-        $ Http.addRequestHeader (fromString "X-CMC_PRO_API_KEY") (fromString $ apiKey rp)
-        $ Http.addRequestHeader (fromString "Accept-Encoding") (fromString "deflate, gzip")
+  let request =
+        Http.setRequestPath (rEndpoint rp) $
+        Http.setRequestQueryString (rqQueryStr rp) $
+        Http.setRequestSecure True $
+        Http.addRequestHeader (fromString "X-CMC_PRO_API_KEY") (fromString $ apiKey rp) $
+        Http.addRequestHeader
+          (fromString "Accept-Encoding")
+          (fromString "deflate, gzip")
         request'
   print $ "Sending new market API request to: " ++ rHost rp ++ "" ++ rEndpoint rp
   print request
--- app/MarketDataClientUtils.hs
+++ app/MarketDataClientUtils.hs
@@ -1,49 +1,52 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module MarketDataClientUtils (
-    resStatus,
+module MarketDataClientUtils
+  ( resStatus,
     resData,
     reqExHandler,
     buildReqQ,
     rHost,
     rEndpoint,
-    rqQueryStr
-) where
+    rqQueryStr,
+  )
+where
 
-import MarketDataClientTypes
-
-import Data.Aeson as JSON
-import qualified Network.HTTP.Simple as Http
-import Control.Monad (MonadPlus(mzero))
-import Data.String (IsString(fromString))
-import Control.Exception (SomeException(SomeException), handle)
-import Network.HTTP.Client.Conduit (HttpException)
-import qualified GHC.Exts as Aeson
-import qualified Network.HTTP.Conduit as Http
+import Control.Exception (SomeException (SomeException), handle)
+import Control.Monad (MonadPlus (mzero))
 import Control.Monad.IO.Class (liftIO)
+import Data.Aeson as JSON
+import Data.String (IsString (fromString))
+import qualified GHC.Exts as Aeson
+import MarketDataClientTypes
+import Network.HTTP.Client.Conduit (HttpException)
+import qualified Network.HTTP.Conduit as Http
+import qualified Network.HTTP.Simple as Http
 
-buildReqQ :: RStart -> Limit -> QueryParams 
-buildReqQ s l = QueryParams{start=s, limit=l}
+buildReqQ :: RStart -> Limit -> QueryParams
+buildReqQ s l = QueryParams {start = s, limit = l}
 
 -- resStatus extracts the Status of Http Response
-resStatus :: Http.Response a -> IO Status 
+resStatus :: Http.Response a -> IO Status
 resStatus r = do return $ codeToStatus s
-  where s = Http.getResponseStatusCode r
+  where
+    s = Http.getResponseStatusCode r
 
 -- resData builds a DataResponse from APIResponse
 resData :: Http.Response APIResponse -> IO DataResponse
 resData r = do
-    status' <- resStatus r
-    return DataResponse {
-      status=status',
-      message=rmessage dr,
-      body=rdata dr
-    }
-  where dr = Http.getResponseBody r
+  status' <- resStatus r
+  return
+    DataResponse
+      { status = status',
+        message = rmessage dr,
+        body = rdata dr
+      }
+  where
+    dr = Http.getResponseBody r
 
--- codeToStatus maps known API response codes into statuses 
+-- codeToStatus maps known API response codes into statuses
 codeToStatus :: Int -> Status
-codeToStatus c  
+codeToStatus c
   | c >= 200 && c < 300 = Successful
   | c >= 300 && c < 400 = RequestRedirected
   | c == 429 = TooManyRequests
@@ -54,28 +57,29 @@ codeToStatus c
 
 -- rHost extracts host from given params or defaults to sandbox API host
 rHost :: IsString p => ReqParams -> p
-rHost rp 
-  | null $ host rp = fromString coinMarketSandboxAPIServerHost 
+rHost rp
+  | null $ host rp = fromString coinMarketSandboxAPIServerHost
   | otherwise = fromString $ host rp
 
--- rEndpoint extracts endpoint from params or defaults to latest listings endpoint 
+-- rEndpoint extracts endpoint from params or defaults to latest listings endpoint
 rEndpoint :: IsString p => ReqParams -> p
-rEndpoint rp 
+rEndpoint rp
   | null $ endpoint rp = fromString cryptoLatestListingsEndpoint
   | otherwise = fromString $ endpoint rp
 
 -- rqParams extracts and parses request query strings
 rqQueryStr :: ReqParams -> Http.Query
-rqQueryStr ReqParams{query=q} = [("limit", Just l)] where 
+rqQueryStr ReqParams {query = q} = [("limit", Just l)]
+  where
     l = fromString . show $ rLimit q
 
-rLimit :: QueryParams -> Limit 
-rLimit QueryParams{limit=l} 
+rLimit :: QueryParams -> Limit
+rLimit QueryParams {limit = l}
   | l > 0 = l
   | otherwise = 100
- 
+
 -- reqExHandler handles exceptions sending a HTTP request
-reqExHandler :: HttpException -> IO DataResponse
-reqExHandler err = do 
+reqExHandler :: SomeException -> IO DataResponse
+reqExHandler err = do
   putStrLn $ "Error sending request: " ++ show err
-  return DataResponse { status = FailToSendReq, message = show err, body=[] }
+  return DataResponse {status = FailToSendReq, message = show err, body = []}
